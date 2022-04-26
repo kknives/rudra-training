366c3e65df07fc90ddcceeba679fcc121a9ec96e
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-04-26 23:50:23 +0530

    motor-control: Switch curses to `nodelay`
    `getch()` doesn't block, so it has a timeout in place, just need to
    clamp the speed values

--- motor-control/control.py
+++ motor-control/control.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 
 from curses import wrapper
+from functools import partial
 import curses
+
+
+def spd_scale(y0, x0, y1, x1, x):
+    # y-y0 = y2-y1/x2-x1 (x-x0)
+    return y0 + ((y1 - y0) * x / (x1 - x0))
 
 
 def main(stdscr):
     curses.noecho()
     curses.cbreak()
+    curses.curs_set(False)
     stdscr.keypad(True)
     stdscr.clear()
+    stdscr.nodelay(True)
+
+    spd_scale_c = partial(spd_scale, 100, 10, 0, 0)
+    w_down_counter = 0
+    speed = spd_scale_c(w_down_counter)
 
     stdscr.addstr(0, curses.COLS // 2, "Motor Control", curses.A_REVERSE)
     while True:
         keystroke = stdscr.getch()
         if keystroke == ord("q"):
             break
-
+        elif keystroke == ord("w"):
+            w_down_counter += 1
+        else:
+            w_down_counter -= 1
+        speed = spd_scale_c(w_down_counter)
+        stdscr.addstr(
+            4, curses.COLS // 2, f"Current speed is {speed}", curses.A_REVERSE
+        )
         stdscr.refresh()
 
     stdscr.refresh()
